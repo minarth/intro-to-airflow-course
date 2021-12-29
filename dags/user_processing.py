73408e0092ce91ca47449a1c0e99a53ffda37bef
Martin <minarth@gmail.com>
2021-12-29 16:14:53 +0100

    3.21 check HTTP API with sensor

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,5 +1,7 @@
 from airflow.models import DAG
+# To get available ops, run: airflow providers list
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+from airflow.providers.http.sensors.http import HttpSensor
 from datetime import datetime
 
 default_args = {
@@ -15,7 +17,7 @@ with DAG('user_processing',
 	# airflow tasks test user_processing creating_table 2021-12-28
 	creating_table = SqliteOperator(
 		task_id='creating_table',
-		sqlite_conn_id='db_sqlite',
+		sqlite_conn_id='db_sqlite',   # defined in admin->connections
 		sql='''
 			CREATE TABLE users (
 				firstname TEXT NOT NULL, 
@@ -26,4 +28,12 @@ with DAG('user_processing',
 				email TEXT NOT NULL PRIMARY KEY
 			);
 		'''		
+	)
+
+	# You can test this by running:
+	# airflow tasks test user_processing is_api_available 2021-12-28
+	is_api_available = HttpSensor(
+		task_id='is_api_available',
+		http_conn_id='user_api',  # defined in admin->connections
+		endpoint='api/'
 	)
