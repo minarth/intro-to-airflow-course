931e74e81891b89e301d6d8fd38cfe7d313d4fc5
Martin <minarth@gmail.com>
2022-01-04 16:51:53 +0100

    5.41 TaskGroups

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -1,10 +1,8 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.subdag import SubDagOperator
+from airflow.utils.task_group import TaskGroup
 
 from datetime import datetime
-
-from subdags.subdag_parallel_dag import subdag_parallel_dag
 
 default_args = {
 	'start_date': datetime(2021, 12, 10)
@@ -22,10 +20,23 @@ with DAG(
 		bash_command='sleep 3'
 	)
 
-	processing = SubDagOperator(
-		task_id='processing_tasks',
-		subdag=subdag_parallel_dag('parallel_dag', 'processing_tasks', default_args)
-	)
+	with TaskGroup('processing_tasks') as processing_tasks:
+		task_2 = BashOperator(
+			task_id='task_2',
+			bash_command='sleep 3'
+		)
+
+		with TaskGroup('spark_tasks') as spark_tasks:
+			task_3 = BashOperator(
+				task_id='task_3',
+				bash_command='sleep 3'
+			)
+
+		with TaskGroup('flink_tasks') as flink_tasks:
+			task_3 = BashOperator(
+				task_id='task_3',
+				bash_command='sleep 3'
+			)
 
 	task_4 = BashOperator(
 		task_id='task_4',
@@ -36,4 +47,4 @@ with DAG(
 	# task_1 >> task_2 >> task_4
 	# task_1 >> task_3 >> task_4
 
-	task_1 >> processing >> task_4	
+	task_1 >> processing_tasks >> task_4	
